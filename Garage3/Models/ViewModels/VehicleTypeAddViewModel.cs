fa57ec7018ebfc4cd1443622949a1d99320b7703
Magnus <Renen@DESKTOP-88ILJBA>
2020-05-19 13:32:43 +0200

    La till VehicleTypeAddVehicleViewModel

---
+++ Garage3/Models/ViewModels/VehicleTypeAddViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage3.Models.ViewModels
+{
+    public class VehicleTypeAddViewModel
+    {
+        public string Name { get; set; }
+        public int Size { get; set; }
+    }
+}
